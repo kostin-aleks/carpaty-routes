d6d4b2caee049bedd8d51d8d64bd75935e680f89
kostin-alex <aleksandr.kostin@gmail.com>
2025-08-01 21:12:13 +0300

    added models and views

--- app/models/mountains.py
+++ app/models/mountains.py
@@ -1,4 +1,25 @@
-from sqlmodel import Field, SQLModel
+from datetime import datetime
+
+from sqlmodel import Field, SQLModel, Relationship
+from sqlalchemy import Column, Text
+from sqlalchemy.types import String, TypeDecorator
+from pydantic import HttpUrl, field_serializer, computed_field, BaseModel, ConfigDict
+from typing import Optional, List
+
+
+class HttpUrlType(TypeDecorator):
+    impl = String(2083)
+    cache_ok = True
+    python_type = HttpUrl
+
+    def process_bind_param(self, value, dialect) -> str:
+        return str(value)
+
+    def process_result_value(self, value, dialect) -> HttpUrl:
+        return HttpUrl(url=value)
+
+    def process_literal_param(self, value, dialect) -> str:
+        return str(value)
 
 
 class GeoPoint(SQLModel, table=True):
@@ -7,6 +28,9 @@ class GeoPoint(SQLModel, table=True):
     latitude: float = 0
     longitude: float = 0
 
+    peaks: List["Peak"] = Relationship(back_populates="point")
+    routepoints: List["RoutePoint"] = Relationship(back_populates="point")
+
 
 class Ridge(SQLModel, table=True):
     """
@@ -14,21 +38,159 @@ class Ridge(SQLModel, table=True):
     """
     __tablename__ = 'ridge'
     id: int | None = Field(default=None, primary_key=True)
-    slug: str | None = Field(default=None)
-    name: str
-    description: str | None = Field(default=None)
+    slug: str | None = Field(default=None, unique=True, max_length=128)
+    name: str = Field(max_length=128)
+    description: str | None = Field(default=None, sa_column=Column(Text))
     # editor = models.ForeignKey(
     #    get_user_model(), on_delete=models.PROTECT, verbose_name=_("editor"), null=True)
     active: bool = Field(default=True)
-    # changed = models.DateTimeField(
-    #    _("created"), default=timezone.now, db_index=True)
+    changed: datetime = Field(default_factory=datetime.utcnow)
+
+    infolinks: List["RidgeInfoLink"] = Relationship(back_populates="ridge")
+    peaks: List["Peak"] = Relationship(back_populates="ridge")
+
+    @computed_field
+    @property
+    def peaks_list(self) -> list:
+        return self.peaks
 
 
-# class GeoPoint(models.Model):
-#     """
-#     GeoPoint stores data related to point on Earth surface
-#     """
-#     latitude = models.DecimalField(
-#         _("latitude"), default=0, decimal_places=6, max_digits=10)
-#     longitude = models.DecimalField(
-#         _("longitude"), default=0, decimal_places=6, max_digits=10)
+class RidgeOut(BaseModel):
+    """
+    Ridge model for single Ridge response
+    """
+    model_config = ConfigDict(from_attributes=True)
+    id: int | None
+    slug: str | None
+    name: str = Field(max_length=128)
+    description: str | None
+    # editor = models.ForeignKey(
+    #    get_user_model(), on_delete=models.PROTECT, verbose_name=_("editor"), null=True)
+    active: bool = Field(default=True)
+    changed: datetime | None
+
+    peaks_list: list | None = []
+
+
+class RidgeInfoLink(SQLModel, table=True):
+    """
+    Ridge Info Link model
+    """
+    __tablename__ = 'ridge_info_link'
+    id: int | None = Field(default=None, primary_key=True)
+    ridge_id: Optional[int] = Field(default=None, foreign_key="ridge.id")
+    ridge: Optional[Ridge] = Relationship(back_populates="infolinks")
+    link: HttpUrl = Field(index=True, unique=True, nullable=False, sa_type=HttpUrlType, max_length=128)
+    description: str | None = Field(default=None, max_length=128)
+
+
+class Peak(SQLModel, table=True):
+    """
+    Peak model
+    """
+    __tablename__ = 'peak'
+    id: int | None = Field(default=None, primary_key=True)
+    slug: str | None = Field(default=None, unique=True, max_length=64)
+    ridge_id: Optional[int] = Field(default=None, foreign_key="ridge.id")
+    ridge: Optional[Ridge] = Relationship(back_populates="peaks")
+    name: str = Field(max_length=64)
+    description: str | None = Field(default=None, sa_column=Column(Text))
+    height: int | None = Field(default=None)
+    point_id: Optional[int] = Field(default=None, foreign_key="geopoint.id")
+    point: Optional[GeoPoint] = Relationship(back_populates="peaks")
+    photo: str | None = Field(default=None, unique=True, max_length=128)
+    # editor
+    active: bool = Field(default=True)
+    changed: datetime = Field(default_factory=datetime.utcnow)
+
+    photos: List["PeakPhoto"] = Relationship(back_populates="peak")
+    routes: List["Route"] = Relationship(back_populates="peak")
+
+#     @field_serializer('photos')
+#     def photos_serialize(photos: list):
+#         return ['a', 'b']
+
+
+class PeakPhoto(SQLModel, table=True):
+    """
+    Peak Photo model
+    """
+    __tablename__ = 'peak_photo'
+    id: int | None = Field(default=None, primary_key=True)
+    peak_id: Optional[int] = Field(default=None, foreign_key="peak.id")
+    peak: Optional[Peak] = Relationship(back_populates="photos")
+    photo: str | None = Field(default=None, unique=True, max_length=128)
+    description: str | None = Field(default=None, max_length=128)
+
+
+class Route(SQLModel, table=True):
+    """
+    Route model
+    """
+    __tablename__ = 'route'
+    id: int | None = Field(default=None, primary_key=True)
+    peak_id: Optional[int] = Field(default=None, foreign_key="peak.id")
+    peak: Optional[Peak] = Relationship(back_populates="routes")
+    name: str = Field(max_length=64)
+    slug: str | None = Field(default=None, unique=True, max_length=64)
+    description: str | None = Field(default=None, sa_column=Column(Text))
+    short_description: str | None = Field(default=None, sa_column=Column(Text))
+    recommended_equipment: str | None = Field(default=None, sa_column=Column(Text))
+    photo: str | None = Field(default=None, unique=True, max_length=128)
+    # map_image
+    difficulty: str | None = Field(default=None, max_length=3)
+    max_difficulty: str | None = Field(default=None, max_length=16)
+    author: str | None = Field(default=None, max_length=64)
+    length: int | None = Field(default=None)
+    year: int | None = Field(default=None)
+    height_difference: int | None = Field(default=None)
+    start_height: int | None = Field(default=None)
+    descent: str | None = Field(default=None, sa_column=Column(Text))
+    # editor
+    changed: datetime = Field(default_factory=datetime.utcnow)
+    ready: bool = Field(default=False)
+
+    photos: List["RoutePhoto"] = Relationship(back_populates="route")
+    routepoints: List["RoutePoint"] = Relationship(back_populates="route")
+    sections: List["RouteSection"] = Relationship(back_populates="route")
+
+
+class RouteSection(SQLModel, table=True):
+    """
+    Route Section model
+    """
+    __tablename__ = 'route_section'
+    id: int | None = Field(default=None, primary_key=True)
+    route_id: Optional[int] = Field(default=None, foreign_key="route.id")
+    route: Optional[Route] = Relationship(back_populates="sections")
+    num: int | None = Field(default=None)
+    description: str | None = Field(default=None, sa_column=Column(Text))
+    length: int | None = Field(default=None)
+    difficulty: str | None = Field(default=None, max_length=32)
+    angle: str | None = Field(default=None, max_length=32)
+
+
+class RoutePhoto(SQLModel, table=True):
+    """
+    Route Photo model
+    """
+    __tablename__ = 'route_photo'
+    id: int | None = Field(default=None, primary_key=True)
+    route_id: Optional[int] = Field(default=None, foreign_key="route.id")
+    route: Optional[Route] = Relationship(back_populates="photos")
+    photo: str | None = Field(default=None, unique=True, max_length=128)
+    description: str | None = Field(default=None, max_length=128)
+
+
+class RoutePoint(SQLModel, table=True):
+    """
+    Route Point model
+    """
+    __tablename__ = 'route_point'
+    id: int | None = Field(default=None, primary_key=True)
+    route_id: Optional[int] = Field(default=None, foreign_key="route.id")
+    route: Optional[Route] = Relationship(back_populates="routepoints")
+    point_id: Optional[int] = Field(default=None, foreign_key="geopoint.id")
+    point: Optional[GeoPoint] = Relationship(back_populates="routepoints")
+    description: str | None = Field(default=None, max_length=128)
+
--- app/routers/mountains.py
+++ app/routers/mountains.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
-from app.models.mountains import GeoPoint, Ridge
+from app.models.mountains import GeoPoint, Ridge, RidgeOut, Peak, Route
 from app.database import get_session
 
 # router = APIRouter()
@@ -13,22 +13,36 @@ router = APIRouter(
 )
 
 
-@router.get("/points")
-async def get_points(session: Session = Depends(get_session)):
-    points = session.exec(select(GeoPoint)).all()
-    return points
+# @router.get("/points")
+# async def get_points(session: Session = Depends(get_session)):
+#     points = session.exec(select(GeoPoint)).all()
+#     return points
 
 
 @router.get("/ridges")
-async def get_ridges(session: Session = Depends(get_session)):
+async def get_ridges(session: Session = Depends(get_session)) -> list[Ridge]:
     ridges = session.exec(select(Ridge)).all()
     return ridges
 
-# @router.get("/users/me", tags=["users"])
-# async def read_user_me():
-#     return {"username": "fakecurrentuser"}
-#
-#
-# @router.get("/users/{username}", tags=["users"])
-# async def read_user(username: str):
-#     return {"username": username}
+
+@router.get("/ridge/{slug}")
+async def get_ridge(slug: str, session: Session = Depends(get_session)) -> RidgeOut:
+    statement = select(Ridge).where(Ridge.slug == slug)
+    ridge = session.exec(statement).first()
+    if ridge is None:
+        raise HTTPException(status_code=404, detail="Ridge not found")
+    ridge_out = RidgeOut.from_orm(ridge)
+
+    return ridge_out
+
+
+@router.get("/peaks")
+async def get_peaks(session: Session = Depends(get_session)):
+    peaks = session.exec(select(Peak)).all()
+    return peaks
+
+
+@router.get("/routes")
+async def get_routes(session: Session = Depends(get_session)):
+    routes = session.exec(select(Route)).all()
+    return routes
